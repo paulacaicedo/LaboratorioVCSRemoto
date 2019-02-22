71bf34276625657f4b50b002e61b6e2f211a72f2
paulacaicedo <paula.caciedo.2019@upb.edu.co>
2019-02-22 17:23:25 -0500

    Solucion

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 print("Programa de Laboratorio")
 
 import math
-numero_b=int(input("Inserte el valor de b,por favor: "))
-numero_d=int(input("Inserte el valor de d,por favor: "))
 
-cuadrado_b=numero_b**4
-raiz=math.sqrt(numero_d)
+print("Caulculo de raices")
+a=int(input("Inserte el valor de a,por favor: "))
+b=int(input("Inserte el valor de b,por favor: "))
+c=int(input("Inserte el valor de c,por favor: "))
 
-print("El cuadrado de b es: " + str(cuadrado_b))
-print("La raiz es: " + str(raiz))
+def discriminante(a,b,c):
+	discrim=pow(b,2)-(4*a*c)
+	return discrim
+
+def raices(a,b,disc):
+	raiz1=(-b+math.sqrt(disc))/(2*a)
+	raiz2=(-b-math.sqrt(disc))/(2*a)
+	return raiz1,raiz2
+
+disc=discriminante(a,b,c)
+disc_igual=(b*-1)/(2*a)
+
+if disc<0:
+	print("No existe solucion en los numeros reales")
+elif disc>0:
+	print("Las raices son: ")
+	print (raices(a,b,disc))
+else:
+	disc==0
+	print("Son iguales")
+	print (disc_igual)
