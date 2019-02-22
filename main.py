32d39b3bd59863cfa3cc9e988543aba512a1d5e0
paulacaicedo <paula.caciedo.2019@upb.edu.co>
2019-02-22 15:57:09 -0500

    mi primer commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Programa de Laboratorio")
+
+numero_a=int(input("Inserte el valor de a,por favor: "))
+numero_c=int(input("Inserte el valor de b,por favor: "))
+
+doble_a=numero_a**2
+producto=numero_a*numero_c
+
+print("El doble de a es: " + str(doble_a))
+print("El producto es: " + str(producto))
